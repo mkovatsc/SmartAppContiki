2bced8fd9c472e974c731a788e4fc765a316a4bf
Christof Baumann <baumachr@student.ethz.ch>
2011-04-18 16:45:00 +0200

    Implemented all thermostat coap functions except timers

--- examples/honeywell/honeywell.c
+++ examples/honeywell/honeywell.c
@@ -35,9 +35,13 @@
 #include <string.h>
 #include <stdlib.h>
 #include <avr/eeprom.h>
+#include <avr/pgmspace.h>
 #include "contiki-raven.h"
 #include "rs232.h"
 #include "ringbuf.h"
+#include "rest.h"
+#include "sys/clock.h"
+
 
 /*---------------------------------------------------------------------------*/
 PROCESS(honeywell_process, "Honeywell comm");
@@ -65,10 +69,10 @@ PROCESS(telnetd_process, "Telnet server");
 #define ISO_cr       0x0d
 
 #ifndef TELNETD_CONF_LINELEN
-#define TELNETD_CONF_LINELEN 40
+#define TELNETD_CONF_LINELEN 110
 #endif
 #ifndef TELNETD_CONF_NUMLINES
-#define TELNETD_CONF_NUMLINES 10
+#define TELNETD_CONF_NUMLINES 1
 #endif
 
 struct telnetd_state {
@@ -111,109 +115,23 @@ static int uart_get_char(unsigned char c)
 	return 1;
 }
 
-
-static void ATInterpreterProcessCommand(char* command)
-{
-	// HACK: dummy commands we need to accept
-	if ((strcmp(command, "ATS01=31f4") == 0) ||
-			(strcmp(command, "ATS00=0001") == 0) ||
-			(strcmp(command, "at+dassl") == 0))
-	{
-		printf("%s\r\n", AT_RESPONSE_OK);
-	}
-	// we need to process the join command
-	else if (strcmp(command, "at+jn") == 0)
-	{
-		printf("JPAN:11,31F4\r\n");
-		printf("%s\r\n", AT_RESPONSE_OK);
-	}
-	// check if the command starts with "AT". if not, we return ERROR
-	else if (strlen(command) < strlen(AT_COMMAND_PREFIX) ||
-			strncmp(command, AT_COMMAND_PREFIX, strlen(AT_COMMAND_PREFIX)) != 0)
-	{
-		printf("%s\r\n", AT_RESPONSE_ERROR);
-	}
-	// check if we have an simple "AT" command
-	else if (strlen(command) == strlen(AT_COMMAND_PREFIX) &&
-			strncmp(command, AT_COMMAND_PREFIX, strlen(AT_COMMAND_PREFIX)) == 0)
-	{
-		printf("%s\r\n", AT_RESPONSE_OK);
-	}
-	// check if we have an simple "ATI" command
-	else if (strcmp(command, AT_COMMAND_INFO) == 0)
-	{
-		printf("I am an AVR Raven Jackdaw pretending to be an ETRX2 Zigbee module.\r\n%s\r\n", AT_RESPONSE_OK);
-	}
-	// check if we have an simple "ATZ" command
-	else if (strncmp(command, AT_COMMAND_RESET, strlen(command)) == 0)
-	{
-		printf("Resetting...\r\n%s\r\n", AT_RESPONSE_OK);
-	}
-	// check if we have a unicast command
-	else if (strncmp(command, AT_COMMAND_UNICAST, strlen(AT_COMMAND_UNICAST)) == 0)
-	{
-		// we need to parse the remaining part
-		char* address = &command[strlen(AT_COMMAND_UNICAST)];
-		char* payload = address;
-		while ( *payload != ',' && *payload != '\0' )
-		{
-			payload++;
-		}
-		if ( *payload == ',' )
-		{
-			*payload = '\0';
-			payload++;
-		}
-
-		// trim CR/LF at the end of the payload
-		int len = strlen(payload);
-		if (len > 0 && (payload[len-1] == '\r' || payload[len-1] == '\n'))
-		{
-			payload[len-1] = '\0';
-			if (len > 1 && (payload[len-2] == '\r' || payload[len-2] == '\n'))
-			{
-				payload[len-2] = '\0';
-			}
-		}
-
-		// replace = ~~ with \r\n
-		int idx = 0;
-		while (payload[idx] != '\0')
-		{
-			if (payload[idx] == '~')
-			{
-				if (idx > 0 && payload[idx-1] == '\r')
-				{
-					payload[idx] = '\n';
-				}
-				else
-				{
-					payload[idx] = '\r';
-				}
-			}
-			idx++;
-		}
-
-		printf("+UCAST:00\r\n%s\r\n", AT_RESPONSE_OK);
-
-		// now we can send the payload to the client
-		telnet( payload );
-
-		// HOST is waiting for "ACK:00" or NACK
-		printf("ACK:00\r\n");
-	}
-	else
-	{
-		// default: we return ERROR
-		printf("%s\r\n", AT_RESPONSE_ERROR);
-	}
-}
+enum mode {manual=0, timers=1, valve=2};
 
 static struct {
+	uint8_t day;
+	uint8_t month;
+	uint8_t year;
+	uint8_t hour;
+	uint8_t minute;
+	uint8_t second;
+
 	uint16_t is_temperature;
 	uint16_t target_temperature;
 	uint16_t battery;
 	uint8_t valve;
+	uint8_t mode;
+
+	clock_time_t last_poll;
 } poll_data;
 
 static void parseD(char * data){
@@ -224,13 +142,31 @@ static void parseD(char * data){
 		poll_data.target_temperature = atoi(&data[43]);
 		poll_data.battery = atoi(&data[51]);
 
-		/*char buf[20];
-		sprintf(buf, "V:%u I:%u S:%u B:%u", poll_data.valve, poll_data.is_temperature, poll_data.target_temperature, poll_data.battery);
-		telnet(buf);*/
+		switch(data[24]){
+			case 'M':
+				poll_data.mode=manual;
+				break;
+			case 'A':
+				poll_data.mode=timers;
+				break;
+			case 'V':
+				poll_data.mode=valve;
+				break;
+		}
+
+		poll_data.day=atoi(&data[6]);
+		poll_data.month=atoi(&data[9]);
+		poll_data.year=atoi(&data[12]);
+
+		poll_data.hour=atoi(&data[15]);
+		poll_data.minute=atoi(&data[18]);
+		poll_data.second=atoi(&data[21]);
+
+		poll_data.last_poll = clock_time();
 	}
 }
 
-
+static int poll_time = 10;
 
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(honeywell_process, ev, data)
@@ -247,12 +183,12 @@ PROCESS_THREAD(honeywell_process, ev, data)
 	rs232_set_input(RS232_PORT_0, uart_get_char);
 	Led1_on(); // red
 
-	etimer_set(&etimer, CLOCK_SECOND * 10);
+	etimer_set(&etimer, CLOCK_SECOND * poll_time);
 
 	while (1) {
 		PROCESS_WAIT_EVENT();
 		if(ev == PROCESS_EVENT_TIMER) {
-			etimer_reset(&etimer);
+			etimer_set(&etimer, CLOCK_SECOND * poll_time);
 			telnet("INFO: polling...\r\nHONEYWELL> ");
 			printf("D\n");
 			poll = true;
@@ -531,7 +467,307 @@ PROCESS_THREAD(telnetd_process, ev, data)
 }
 /*---------------------------------------------------------------------------*/
 
+
+/* For each resource defined, there corresponds an handler method which should be defined too.
+ * Name of the handler method should be [resource name]_handler
+ * */
+RESOURCE(temperature, METHOD_GET, "temperature");
+void temperature_handler(REQUEST* request, RESPONSE* response)
+{
+	char temp[128];
+	sprintf_P(temp, PSTR("%d.%02d"), poll_data.is_temperature/100, poll_data.is_temperature%100);
+
+	rest_set_header_content_type(response, TEXT_PLAIN);
+	rest_set_response_payload(response, temp, strlen(temp));
+}
+
+RESOURCE(battery, METHOD_GET, "battery");
+void battery_handler(REQUEST* request, RESPONSE* response)
+{
+	char temp[128];
+	sprintf_P(temp, PSTR("%d"), poll_data.battery);
+
+	rest_set_header_content_type(response, TEXT_PLAIN);
+	rest_set_response_payload(response, temp, strlen(temp));
+}
+
+
+
+RESOURCE(mode, METHOD_GET | METHOD_POST, "mode");
+void mode_handler(REQUEST* request, RESPONSE* response)
+{
+	char temp[128];
+	if (rest_get_method_type(request)==METHOD_GET){
+		switch(poll_data.mode){
+			case manual:
+				sprintf_P(temp, PSTR("manual"));
+				break;
+			case timers:
+				sprintf_P(temp, PSTR("auto"));
+				break;
+			case valve:
+				sprintf_P(temp, PSTR("valve"));
+				break;
+			default:
+				sprintf_P(temp, PSTR("Undefined"));
+		}
+	}
+	else{
+		char string[8];
+		if(rest_get_post_variable(request, "mode", string, 7) == 0){ 
+			rest_set_response_status(response, BAD_REQUEST_400);
+			sprintf_P(temp, PSTR("Payload format: mode={auto, manual, valve}"));
+		}
+		else{
+			sprintf_P(temp, PSTR("New mode is: %s"), string);
+			if(strcmp_P(string,PSTR("manual"))==0){
+				printf_P(PSTR("M00\n"));
+			}
+			else if(strcmp_P(string,PSTR("auto"))==0){
+				printf_P(PSTR("M01\n"));
+			}
+			else if(strcmp_P(string,PSTR("valve"))==0){
+				printf_P(PSTR("M02\n"));
+			}
+			else{
+				sprintf_P(temp, PSTR("Payload format: mode={auto, manual, valve}"));
+			}
+		}
+
+	}
+	rest_set_header_content_type(response, TEXT_PLAIN);
+	rest_set_response_payload(response, temp, strlen(temp));
+}
+
+
+RESOURCE(target, METHOD_GET | METHOD_POST, "target");
+void target_handler(REQUEST* request, RESPONSE* response)
+{	
+	char temp[128];
+	if (rest_get_method_type(request)==METHOD_GET){
+		sprintf_P(temp, PSTR("%d.%02d"), poll_data.target_temperature/100, poll_data.target_temperature%100);
+	}
+	else{
+		char string[5];
+		if(rest_get_post_variable(request, "value", string, 4) == 0){ 
+			rest_set_response_status(response, BAD_REQUEST_400);
+			sprintf_P(temp, PSTR("Payload format: value=abc, eg: value=155 sets the temperature 15.5 degrees"));
+		}
+		else{
+			uint16_t value = atoi(string);
+			printf("A%02x\n",value/5);
+			sprintf_P(temp, PSTR("Successfully set value"));
+		}
+	}
+
+	rest_set_header_content_type(response, TEXT_PLAIN);
+	rest_set_response_payload(response, temp, strlen(temp));
+}
+
+RESOURCE(poll, METHOD_GET | METHOD_POST, "poll");
+void poll_handler(REQUEST* request, RESPONSE* response)
+{	
+	char temp[128];
+	if (rest_get_method_type(request)==METHOD_GET){
+		sprintf_P(temp, PSTR("%d"), poll_time);
+	}
+	else{
+		char string[5];
+		if(rest_get_post_variable(request, "value", string, 4) == 0){ 
+			rest_set_response_status(response, BAD_REQUEST_400);
+			sprintf_P(temp, PSTR("Payload format: value=aa, eg: value=15 sets the poll intervall to 15 seconds"));
+		}
+		else{
+			poll_time = atoi(string);
+			sprintf_P(temp, PSTR("Successfully set poll intervall"));
+		}
+	}
+
+	rest_set_header_content_type(response, TEXT_PLAIN);
+	rest_set_response_payload(response, temp, strlen(temp));
+}
+
+RESOURCE(valve, METHOD_GET | METHOD_POST, "valve");
+void valve_handler(REQUEST* request, RESPONSE* response)
+{	
+	char temp[128];
+	if (rest_get_method_type(request)==METHOD_GET){
+		sprintf_P(temp, PSTR("%d"), poll_data.valve);
+	}
+	else{
+		char string[5];
+		if(rest_get_post_variable(request, "value", string, 4) == 0){
+			rest_set_response_status(response, BAD_REQUEST_400);
+			sprintf_P(temp, PSTR("Payload format: value=aa, eg: value=47 sets the valve 47 percent"));
+		}
+		else{
+			int new_valve=atoi(string);
+			printf_P(PSTR("E%02x\n"),new_valve);
+			sprintf_P(temp, PSTR("Successfully set valve position"));
+		}
+	}
+
+	rest_set_header_content_type(response, TEXT_PLAIN);
+	rest_set_response_payload(response, temp, strlen(temp));
+}
+
+RESOURCE(date, METHOD_GET | METHOD_POST, "date");
+void date_handler(REQUEST* request, RESPONSE* response)
+{	
+	char temp[128];
+	if (rest_get_method_type(request)==METHOD_GET){
+		sprintf_P(temp, PSTR("%02d.%02d.%02d"), poll_data.day, poll_data.month, poll_data.year);
+	}
+	else{
+		char string[16];
+		int success = 1;
+		if(rest_get_post_variable(request, "value", string, 10) == 0){
+			success = 0;
+		}
+		else{
+			if(strlen(string)==8){
+				int day=atoi(&string[0]);
+				int month=atoi(&string[3]);
+				int year=atoi(&string[6]);
+
+				if (!(isdigit(string[0]) &&  isdigit(string[1]) && isdigit(string[3]) && isdigit(string[4]) && isdigit(string[6]) && isdigit(string[7]))){
+					success=0;
+				} 
+				else if (!(0<=year && year <=99 && 1<=month && month<=12 && 1<=day )){
+					success=0;
+				}
+				else if( (month==4 || month ==6 || month==9 || month==11) && day>30){
+					success=0;
+				}
+				else if( month==2 && !((year%4)==0) && day > 28) {
+					success=0;
+				}
+				else if( month==2 && day>29){
+					success=0;
+				}
+				else if( day > 31){
+					success=0;
+				}
+
+				if(success){
+					printf_P(PSTR("Y%02x%02x%02x\n"),year,month,day);
+					sprintf_P(temp, PSTR("Successfully set date"));
+				}
+			}
+			else{
+				success = 0;
+			}
+		}
+		if(!success){
+			rest_set_response_status(response, BAD_REQUEST_400);
+			sprintf_P(temp, PSTR("Payload format: value=dd.mm.yy"));
+		}
+	}
+
+	rest_set_header_content_type(response, TEXT_PLAIN);
+	rest_set_response_payload(response, temp, strlen(temp));
+}
+
+
+RESOURCE(time, METHOD_GET | METHOD_POST, "time");
+void time_handler(REQUEST* request, RESPONSE* response)
+{	
+	char temp[128];
+	if (rest_get_method_type(request)==METHOD_GET){
+		clock_time_t now = clock_time();
+		int second = poll_data.second + (now - poll_data.last_poll) / CLOCK_SECOND;
+		int minute = poll_data.minute + (second / 60);
+		int hour = poll_data.hour + (minute / 60);
+		sprintf_P(temp, PSTR("%02d:%02d:%02d"), hour % 24, minute % 60, second % 60 );
+	}
+	else{
+		char string[16];
+		int success = 1;
+		if(rest_get_post_variable(request, "value", string, 10) == 0){
+			success = 0;
+		}
+		else{
+			int length = strlen(string);
+			if(length==8 || length==5){
+				int hour=atoi(&string[0]);
+				int minute=atoi(&string[3]);
+				int second=(length==5)?0:atoi(&string[6]);
+
+				if (length==8 && ! (isdigit(string[6]) && isdigit(string[7]))){
+					success = 0;
+				}
+				else if (!(isdigit(string[0]) &&  isdigit(string[1]) && isdigit(string[3]) && isdigit(string[4]))){
+					success = 0;
+				}
+				else if (!( 0<=hour && hour<=23 && 0<=minute && minute<=59 && 0<=second && second<=59)){
+					success = 0; 
+				}
+
+				if(success){
+					printf_P(PSTR("H%02x%02x%02x\n"),hour,minute,second);
+					sprintf_P(temp, PSTR("Successfully set time"));
+				}
+			}
+			else{
+				success = 0;
+			}
+		}
+		if(!success){
+			rest_set_response_status(response, BAD_REQUEST_400);
+			sprintf_P(temp, PSTR("Payload format: value=mm:hh[:ss]"));
+		}
+	}
+
+	rest_set_header_content_type(response, TEXT_PLAIN);
+	rest_set_response_payload(response, temp, strlen(temp));
+}
+
+
+
+RESOURCE(discover, METHOD_GET, ".well-known/core");
+void discover_handler(REQUEST* request, RESPONSE* response)
+{
+	char temp[128];
+	int index = 0;
+	index += sprintf_P(temp + index, PSTR("</temperature>"));
+	index += sprintf_P(temp + index, PSTR("</target>"));
+	index += sprintf_P(temp + index, PSTR("</mode>"));
+	index += sprintf_P(temp + index, PSTR("</poll>"));
+	index += sprintf_P(temp + index, PSTR("</valve>"));
+	index += sprintf_P(temp + index, PSTR("</battery>"));
+	index += sprintf_P(temp + index, PSTR("</date>"));
+	index += sprintf_P(temp + index, PSTR("</time>"));
+
+
+	rest_set_response_payload(response, temp, strlen(temp));
+	rest_set_header_content_type(response, APPLICATION_LINK_FORMAT);
+}
+
+
+PROCESS(rest_server_example, "Rest Server Example");
+PROCESS_THREAD(rest_server_example, ev, data)
+{
+	PROCESS_BEGIN();
+
+	rest_init();
+	
+	rest_activate_resource(&resource_date);
+	rest_activate_resource(&resource_time);
+	rest_activate_resource(&resource_temperature);
+	rest_activate_resource(&resource_battery);
+	rest_activate_resource(&resource_target);
+	rest_activate_resource(&resource_mode);
+	rest_activate_resource(&resource_poll);
+	rest_activate_resource(&resource_valve);
+	rest_activate_resource(&resource_discover);
+
+	PROCESS_END();
+}
+
+
+
+
 /*---------------------------------------------------------------------------*/
-AUTOSTART_PROCESSES(&telnetd_process, &honeywell_process);
+AUTOSTART_PROCESSES(&telnetd_process, &honeywell_process, &rest_server_example);
 /*---------------------------------------------------------------------------*/
 
