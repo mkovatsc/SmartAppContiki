bfb506f358cf1e7f0f4220b68a7fa5b46fa591cc
Nico Eigenmann <ne87@gmx.net>
2012-07-25 14:34:40 +0200

    Minor changes in Temp-Sensor

--- projects/temp-sensor/temp-sensor.c
+++ projects/temp-sensor/temp-sensor.c
@@ -70,12 +70,12 @@ static struct ringbuf uart_buf;
 static unsigned char uart_buf_data[128] = {0};
 
 static int16_t temperature;
-static int16_t threshold;
 static process_event_t get_temperature_response_event;
 static process_event_t changed_temperature_event;
 clock_time_t last_temperature_reading;
 
-static uint8_t poll_time = 15;
+static int16_t threshold = 10;
+static uint8_t poll_time = 5;
 
 const uint16_t lookup[] PROGMEM = {-285,-54,84,172,238,291,336,375,410,442,471,498,523,547,569,590,611,630,649,667,684,701,718,733,749,764,779,793,808,821,835,849,862,875,888,900,913,925,938,950,962,974,986,998,1009,1021,1033,1044,1056,1067,1079,1090,1102,1113,1124,1136,1147,1158,1170,1181,1193,1204,1216,1227,1239};
 
@@ -101,6 +101,8 @@ static int uart_get_char(unsigned char c)
 
 static void read_temperature(void){
 	static int16_t reading;
+	int16_t new_temperature;
+	
 	adc_init(ADC_CHAN_ADC2, ADC_MUX5_0,ADC_TRIG_FREE_RUN, ADC_REF_INT_1_6, ADC_PS_32, ADC_ADJ_RIGHT);
 	reading = doAdc(ADC_CHAN_ADC2, ADC_MUX5_0, 1);
 	
@@ -110,7 +112,6 @@ static void read_temperature(void){
 	temp = 1 / (0.001129148 + (0.000234125 * temp) + (8.76741e-8 * temp * temp * temp) );
 	temp = temp-273.15;
 	------*/
-	int16_t new_temperature;
 /*	Method without lookup table, inaccurate
 	if (reading < 21){
 		new_temperature = (25*reading-26*15-5*25)*10/15;
@@ -142,8 +143,6 @@ static void read_temperature(void){
 		new_temperature = (int16_t) temp;
 	}
 */	
-	adc_deinit();
-
 	int16_t delta = reading % 16;
 	int16_t low = pgm_read_word(&lookup[reading/16]);
 	int16_t high = pgm_read_word(&lookup[(reading/16)+1]);
@@ -171,7 +170,6 @@ PROCESS_THREAD(rfnode_test_process, ev, data)
 	int buf_pos;
 	char buf[128];
 	static struct etimer etimer;
-	threshold = 10;
 
 
 	ringbuf_init(&uart_buf, uart_buf_data, sizeof(uart_buf_data));
@@ -291,47 +289,110 @@ void temperature_finalize_handler() {
 RESOURCE(poll, METHOD_GET | METHOD_PUT, "config/poll", "title=\"Polling interval\";ct=0;rt=\"time:s\"");
 void poll_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
-  if (REST.get_method_type(request)==METHOD_GET)
-  {
-    snprintf_P((char*)buffer, preferred_size, PSTR("%d"), poll_time);
-  }
-  else
-  {
-    const uint8_t * string = NULL;
-    int success = 1;
-    int len = coap_get_payload(request, &string);
-    if(len == 0){
-            success = 0;
-    }
-    else
-      {
-            int i;
-            for(i=0; i<len; i++){
-                    if (!isdigit(string[i])){
-                            success = 0;
-                            break;
-                    }
-            }
-            if(success){
-                    int poll_intervall = atoi((char*)string);
-                    if(poll_intervall < 255 && poll_intervall > 0){
-                            poll_time = poll_intervall;
-                            strncpy_P((char*)buffer, PSTR("Successfully set poll intervall"), preferred_size);
-                    }
-                    else{
-                            success = 0;
-                    }
-            }
-    }
-    if(!success){
-            REST.set_response_status(response, REST.status.BAD_REQUEST);
-            strncpy_P((char*)buffer, PSTR("Payload format: aa, e.g. 15 sets the poll interval to 15 seconds"), preferred_size);
-    }
-  }
+	if (REST.get_method_type(request)==METHOD_GET)
+	{
+		snprintf_P((char*)buffer, preferred_size, PSTR("%d"), poll_time);
+	}
+	else
+	{
+		const uint8_t * string = NULL;
+		int success = 1;
+		int len = coap_get_payload(request, &string);
+		if(len == 0){
+			success = 0;
+		}
+		else
+		{
+    			int i;
+        		for(i=0; i<len; i++){
+        			if (!isdigit(string[i])){
+                        		success = 0;
+                        		break;
+                	  	}
+            		}
+            		if(success){
+                		int poll_intervall = atoi((char*)string);
+                		if(poll_intervall < 255 && poll_intervall > 0){
+                			poll_time = poll_intervall;
+        				REST.set_response_status(response, REST.status.CHANGED);
+                	        	strncpy_P((char*)buffer, PSTR("Successfully set poll intervall"), preferred_size);
+                	    	}
+                	    	else{
+                	        	success = 0;
+                    		}
+            		}
+    		}
+		if(!success){
+        		REST.set_response_status(response, REST.status.BAD_REQUEST);
+			strncpy_P((char*)buffer, PSTR("Payload format: aa, e.g. 15 sets the poll interval to 15 seconds"), preferred_size);
+		}
+	}
 
-  REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
-  REST.set_response_payload(response, buffer, strlen((char*)buffer));
+	REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
+	REST.set_response_payload(response, buffer, strlen((char*)buffer));
 }
+
+
+/*--------- Threshold ---------------------------------------------------------*/
+RESOURCE(threshold, METHOD_GET | METHOD_PUT, "config/threshold", "title=\"Threshold temperature\";ct=0;rt=\"temperature:C\"");
+void threshold_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+	if (REST.get_method_type(request)==METHOD_GET)
+	{
+		snprintf_P((char*)buffer, preferred_size, PSTR("%d.%01d"), threshold/10, threshold%10);
+		REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
+		REST.set_response_payload(response, buffer, strlen((char*)buffer));
+	}
+	else {
+        	const uint8_t * string = NULL;
+        	int success = 1;
+        	int len = coap_get_payload(request, &string);
+		uint16_t value;
+		if(len == 2){
+			if (isdigit(string[0]) && isdigit(string[1])){
+				value = (atoi((char*) string)) * 10;
+			}
+			else {
+				success = 0;
+			}
+		}
+		else if (len == 3) {
+			if (isdigit(string[0]) && isdigit(string[2]) && string[1]=='.'){
+				value = (atoi((char*) string)) * 10;
+				value += atoi((char*) string+2);
+			}
+			else {
+				success = 0;
+			}
+		}
+		else if(len == 4){
+			if (isdigit(string[0]) && isdigit(string[1]) && isdigit(string[3]) && string[2]=='.'){
+				value = (atoi((char*) string) *10);
+				value += atoi((char*) string+3);
+			}
+			else {
+				success = 0;
+			}
+		}
+		else {
+			success = 0;
+		}
+	        if(success){
+			threshold=value;
+        		REST.set_response_status(response, REST.status.CHANGED);
+			REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
+	
+	        }
+        	else{
+        		REST.set_response_status(response, REST.status.BAD_REQUEST);
+                	strncpy_P((char*)buffer, PSTR("Payload format: tt.t, e.g. 1.0 sets the threshold to 1.0 deg"), preferred_size);
+			REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
+			REST.set_response_payload(response, buffer, strlen((char*)buffer));
+			return;
+        	}
+	}
+}
+
 
 
 
@@ -342,6 +403,7 @@ PROCESS_THREAD(coap_process, ev, data)
 
 	rest_init_engine();
 	rest_activate_event_resource(&resource_temperature);	
+	rest_activate_resource(&resource_threshold);
 	rest_activate_resource(&resource_poll);
 
 	//activate the resources
